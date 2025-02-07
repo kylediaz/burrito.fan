9b4d4b4894e9eb02a0487fdce046339f3eb59c4c
Kyle Diaz <dev@kylediaz.com>
2025-02-07 14:15:42 -0500

    fix: Marker focus change collides with scroll events

--- app/BurritoReviews.tsx
+++ app/BurritoReviews.tsx
@@ -6,6 +6,7 @@ import React, {
   useRef,
   useEffect,
   RefObject,
+  useState,
 } from "react";
 import { BurritoReviewModel, FocusedEntry } from "./types";
 import Header from "@/components/Header";
@@ -15,12 +16,19 @@ import styles from "./BurritoReviews.module.scss";
 
 function BurritoReviews(props: Props) {
   const { burritos, focusedEntry, setFocusedEntry } = props;
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [ignoreScroll, setIgnoreScroll] = useState(false);
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const containerRef: RefObject<HTMLDivElement | null> = useRef(null);
 
-  const reviewElementsRefs = new Array<RefObject<HTMLElement>>(burritos.length);
+  const reviewElementsRefs = new Array<RefObject<HTMLElement | null>>(
+    burritos.length,
+  );
   const reviewElements = new Array<React.JSX.Element>(burritos.length);
   for (let index = 0; index < burritos.length; index++) {
     const burrito = burritos[index];
-    const ref: RefObject<HTMLElement> = useRef(null);
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const ref: RefObject<HTMLLIElement | null> = useRef<HTMLLIElement>(null);
     reviewElementsRefs[index] = ref;
     reviewElements[index] = (
       <li key={index} id={`burrito-review-${index}`} ref={ref}>
@@ -32,17 +40,32 @@ function BurritoReviews(props: Props) {
     );
   }
 
-  const handleScroll = (e) => {
-    console.log("scroll");
+  const handleScroll = () => {
+    if (ignoreScroll || containerRef.current == null) {
+      return;
+    }
+
+    const containerHeight = containerRef.current.getBoundingClientRect().height;
+    let closestEntry = -1;
+    let closestDistanceToCenter = Infinity;
     for (let index = 0; index < burritos.length; index++) {
       const ref = reviewElementsRefs[index];
+      if (ref.current == null) {
+        continue;
+      }
       const rect = ref.current.getBoundingClientRect();
-      const middle = window.innerHeight / 2;
-      const inMiddle = Math.abs(rect.top - (middle - rect.height / 2)) < 5;
-      if (inMiddle && focusedEntry.idx != index) {
-        setFocusedEntry({ idx: index, source: "scroll" });
+      const rectCenter = (rect.top + rect.bottom) / 2;
+
+      const distanceToCenter = Math.abs(rectCenter - containerHeight / 2);
+      if (
+        distanceToCenter < closestDistanceToCenter &&
+        distanceToCenter < 300
+      ) {
+        closestEntry = index;
+        closestDistanceToCenter = distanceToCenter;
       }
     }
+    setFocusedEntry({ idx: closestEntry, source: "scroll" });
   };
 
   useEffect(() => {
@@ -53,22 +76,40 @@ function BurritoReviews(props: Props) {
     ) {
       return;
     }
+    if (focusedEntry.source == "map") {
+      setIgnoreScroll(true);
+    }
     const element = document.getElementById(
       `burrito-review-${focusedEntry.idx}`,
     );
     element?.scrollIntoView({
       behavior: "smooth",
-      block: "end",
-      inline: "nearest",
+      block: "center",
+      inline: "center",
     });
   }, [focusedEntry]);
 
   return (
-    <div className={styles.outerContainer} onScroll={handleScroll}>
-      <div className={styles.innerContainer}>
+    <div
+      ref={containerRef}
+      className={styles.outerContainer + " mx-2"}
+      onScroll={handleScroll}
+      onWheel={() => setIgnoreScroll(false)}
+      onTouchStart={() => setIgnoreScroll(false)}
+    >
+      <div id="reviews" className={styles.innerContainer}>
         <Header burritos={burritos} />
         <ul>{reviewElements}</ul>
       </div>
+      <footer
+        className="flex flex-col justify-center items-center"
+        style={{ height: "40vh" }}
+      >
+        <div>Thanks for reading :)</div>
+        <div>
+          <a href="#reviews">Back to top ↑</a>
+        </div>
+      </footer>
     </div>
   );
 }
