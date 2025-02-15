4146f1b6232aedd7f5f1fc00a37bb6d0ebf070d8
Kyle Diaz <dev@kylediaz.com>
2025-02-15 15:07:12 -0500

    nit: move const calculation to util

--- app/util.tsx
+++ app/util.tsx
@@ -4,3 +4,11 @@ export function onMobile(): boolean {
 export function formatNumberWithCommas(number: number): string {
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
+
+const gramsPerGrain = 0.04;
+const gramsOfRicePerCup = 200;
+const grainsPerCupOfRice = gramsOfRicePerCup / gramsPerGrain;
+const cupsOfRicePerBurrito = 0.25;
+const grainsOfRicePerBurrito = cupsOfRicePerBurrito * grainsPerCupOfRice;
+const metersPerGrain = 0.0075;
+export const metersOfRicePerBurrito = metersPerGrain * grainsOfRicePerBurrito;
