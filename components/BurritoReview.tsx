bcebff564658df1cc545ff5d4aca027ee7548f1d
Kyle Diaz <dev@kylediaz.com>
2025-02-14 22:15:38 -0500

    nit: Remove dead code

--- components/BurritoReview.tsx
+++ components/BurritoReview.tsx
@@ -3,23 +3,6 @@ import React from "react";
 import styles from "./BurritoReview.module.scss";
 import { BurritoReviewModel } from "../app/types";
 
-const Rating = ({ number }: { number: number }) => {
-  return (
-    <div className="flex items-center space-x-1 bg-black text-white px-3 py-2">
-      <span className="text-2xl font-bold">{Math.floor(number)}</span>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="16"
-        height="16"
-        viewBox="0 0 24 24"
-        fill="white"
-      >
-        <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
-      </svg>
-    </div>
-  );
-};
-
 export interface Props {
   burrito: BurritoReviewModel;
 }
